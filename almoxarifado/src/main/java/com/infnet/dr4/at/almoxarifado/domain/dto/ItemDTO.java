10225d538561043680455d1396abf06fa92d5205
Raphael Van Well <rapha.well@outlook.com>
2025-12-08 15:53:57 -0300

    removido ItemDTO e ajuste no PedidoFechadoEvent

--- almoxarifado/src/main/java/com/infnet/dr4/at/almoxarifado/domain/dto/ItemDTO.java
+++
@@ -1,6 +0,0 @@
-package com.infnet.dr4.at.almoxarifado.domain.dto;
-
-import lombok.Data;
-
-public record ItemDTO( String sku,  int quantidade) {
-}
--- almoxarifado/src/main/java/com/infnet/dr4/at/almoxarifado/events/PedidoFechadoEvent.java
+++ almoxarifado/src/main/java/com/infnet/dr4/at/almoxarifado/events/PedidoFechadoEvent.java
@@ -1,11 +1,9 @@
 package com.infnet.dr4.at.almoxarifado.events;
 
-import com.infnet.dr4.at.almoxarifado.domain.dto.ItemDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
 import java.util.UUID;
 
 @Data
